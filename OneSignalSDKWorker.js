a88d3a40c2d0cce44c78f1a5b972bdf098c2853d
mattlin777 <40792942+mattlin777@users.noreply.github.com>
2025-03-31 23:50:00 +0800

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,2 @@
 // importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-importScripts("https://jochihwz888.github.io/page-test/myOneSignal.sw.js");
+importScripts("https://mattlin777.github.io/test/myOneSignal.sw.js");
